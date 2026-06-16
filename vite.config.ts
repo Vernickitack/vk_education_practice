71baabdea23f3a52f5726f93faba413aa41e7fec
Nikita Veretelnikov <113236698+Ver-Nick@users.noreply.github.com>
2026-06-17 02:30:51 +0300

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/final_enhancement_project/',
+  base: '/vk_education_practice/',
   plugins: [react()]
 })
